6e6f45a742befc4dee9fa588187a35be847ad966
jonasa64 <hvidovrefifa@gmail.com>
2021-05-16 23:42:37 +0200

    cors updates

--- recipe-api/config/cors.php
+++ recipe-api/config/cors.php
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'api/login', 'api/logout', 'sanctum/csrf-cookie', '/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT','*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000','http://localhost:8000','*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['localhost:3000\/'],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['X-Custom-Header', 'Upgrade-Insecure-Requests','Authorization', '*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
