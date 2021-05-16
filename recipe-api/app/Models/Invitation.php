6670d62871b9dfc03b493ec924879d9f39f58f8f
jonasa64 <hvidovrefifa@gmail.com>
2021-05-16 23:42:17 +0200

    Update Invitation.php

--- recipe-api/app/Models/Invitation.php
+++ recipe-api/app/Models/Invitation.php
@@ -4,15 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\MediaLibrary\HasMedia;
 
-
-class Invitation extends Model implements HasMedia
+class Invitation extends Model
 {
     protected $guarded = [];
 
-    use HasFactory, InteractsWithMedia;
+    use HasFactory;
 
     public function user(){
         return $this->belongsTo(User::class);
@@ -20,15 +17,6 @@ class Invitation extends Model implements HasMedia
 
     public function invitedPerson(){
         return $this->hasMany(InvitedPerson::class);
-    }
-
-    public function image(){
-        if($this->media->first()){
-            return $this->media->first()->getFullUrl();
-        }
-
-        return null;
-
     }
 
     public function isOwner(){
